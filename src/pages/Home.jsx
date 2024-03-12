d9b9fd038807f6f0e092b79757860ca6fb1df23e
thisisal1ev <aaalievvv1@gmail.com>
2024-03-12 15:53:30 +0500

    add accordion

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,15 +33,9 @@ import faqImg from '../assets/img/woman-writing-on-paper-3228878.png'
 
 function Icon({ id, open }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={2}
-      stroke="currentColor"
-      className={`${id === open ? "rotate-180" : ""} h-5 w-5 transition-transform`}
-    >
-      <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+    <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24"
+      className={`${id === open ? "rotate-45" : ""} h-5 w-5 transition-transform`}>
+      <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M20 12h-8m0 0H4m8 0V4m0 8v8" />
     </svg>
   );
 }
@@ -232,37 +226,40 @@ const Home = () => {
 
       <div className="py-32 bg-grey">
         <div className="w-full max-w-7xl mx-auto px-5 flex items-start justify-between">
-          <div className="space-y-4">
+          <div className="space-y-4 mr-[109px]">
             <h2 className="font-semibold text-4xl leading-[56px]">Frequently asked questions</h2>
             <Link to='/contact' className="inline-block text-royalBlue font-medium text-base leading-8">Contact us for more info</Link>
           </div>
-          <div className="flex flex-col">
-            <Accordion  open={open === 1} icon={<Icon id={1} open={open} />}>
-              <AccordionHeader onClick={() => handleOpen(1)}>What is Material Tailwind?</AccordionHeader>
-              <AccordionBody>
-                We&apos;re not always in the position that we want to be at. We&apos;re constantly
-                growing. We&apos;re constantly making mistakes. We&apos;re constantly trying to express
-                ourselves and actualize our dreams.
+          <div className="flex flex-col w-full">
+
+            <Accordion open={open === 1} icon={<Icon id={1} open={open} />}>
+              <AccordionHeader onClick={() => handleOpen(1)}><p className="text-2xl font-medium leading-9"><span className="inline-block font-normal text-royalBlue text-2xl leading-9 mr-5">01</span>How much time does it take?</p></AccordionHeader>
+              <AccordionBody className='text-lg font-medium leading-8 pl-12'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do<br /> eiusmod tempor incididunt ut labore et dolore magna aliqua.o
               </AccordionBody>
             </Accordion>
             <Accordion open={open === 2} icon={<Icon id={2} open={open} />}>
-              <AccordionHeader onClick={() => handleOpen(2)}>
-                How to use Material Tailwind?
-              </AccordionHeader>
-              <AccordionBody>
-                We&apos;re not always in the position that we want to be at. We&apos;re constantly
-                growing. We&apos;re constantly making mistakes. We&apos;re constantly trying to express
-                ourselves and actualize our dreams.
+              <AccordionHeader onClick={() => handleOpen(2)}><p className="text-2xl font-medium leading-9"><span className="inline-block font-normal text-royalBlue text-2xl leading-9 mr-5">01</span>How much time does it take?</p></AccordionHeader>
+              <AccordionBody className='text-lg font-medium leading-8 pl-12'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do<br /> eiusmod tempor incididunt ut labore et dolore magna aliqua.o
               </AccordionBody>
             </Accordion>
             <Accordion open={open === 3} icon={<Icon id={3} open={open} />}>
-              <AccordionHeader onClick={() => handleOpen(3)}>
-                What can I do with Material Tailwind?
-              </AccordionHeader>
-              <AccordionBody>
-                We&apos;re not always in the position that we want to be at. We&apos;re constantly
-                growing. We&apos;re constantly making mistakes. We&apos;re constantly trying to express
-                ourselves and actualize our dreams.
+              <AccordionHeader onClick={() => handleOpen(3)}><p className="text-2xl font-medium leading-9"><span className="inline-block font-normal text-royalBlue text-2xl leading-9 mr-5">01</span>How much time does it take?</p></AccordionHeader>
+              <AccordionBody className='text-lg font-medium leading-8 pl-12'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do<br /> eiusmod tempor incididunt ut labore et dolore magna aliqua.o
+              </AccordionBody>
+            </Accordion>
+            <Accordion open={open === 4} icon={<Icon id={4} open={open} />}>
+              <AccordionHeader onClick={() => handleOpen(4)}><p className="text-2xl font-medium leading-9"><span className="inline-block font-normal text-royalBlue text-2xl leading-9 mr-5">01</span>How much time does it take?</p></AccordionHeader>
+              <AccordionBody className='text-lg font-medium leading-8 pl-12'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do<br /> eiusmod tempor incididunt ut labore et dolore magna aliqua.o
+              </AccordionBody>
+            </Accordion>
+            <Accordion open={open === 5} icon={<Icon id={5} open={open} />}>
+              <AccordionHeader onClick={() => handleOpen(5)}><p className="text-2xl font-medium leading-9"><span className="inline-block font-normal text-royalBlue text-2xl leading-9 mr-5">01</span>How much time does it take?</p></AccordionHeader>
+              <AccordionBody className='text-lg font-medium leading-8 pl-12'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do<br /> eiusmod tempor incididunt ut labore et dolore magna aliqua.o
               </AccordionBody>
             </Accordion>
           </div>
